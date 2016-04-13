010991c92fad3c7995a4ad84aa844df18114fa9f
richegg <zxp86021@gmail.com>
2016-04-13 13:14:45 +0800

    finish status command

--- app/Commands/DoorStatusCommand.php
+++ app/Commands/DoorStatusCommand.php
@@ -22,10 +22,20 @@ class DoorStatusCommand extends Command
      */
     public function handle($arguments)
     {
+        $firebase = new \Firebase\FirebaseLib(env('FIREBASE'));
+        $status = $firebase->get('/status');
+
+        if ($status == 1) {
+            $reply = 'MOLi 關門中';
+        } else if ($status == 0) {
+            $reply = 'MOLi 開門中';
+        } else {
+            $reply = '我現在 GG 中 Orz';
+        }
         // This will send a message using `sendMessage` method behind the scenes to
         // the user/chat id who triggered this command.
         // `replyWith<Message|Photo|Audio|Video|Voice|Document|Sticker|Location|ChatAction>()` all the available methods are dynamically
         // handled when you replace `send<Method>` with `replyWith` and use the same parameters - except chat_id does NOT need to be included in the array.
-        $this->replyWithMessage(['text' => 'door status']);
+        $this->replyWithMessage(['text' => $reply]);
     } 
 }
