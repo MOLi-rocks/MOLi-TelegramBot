4328255cbae77ed358c864bf6e198058e301075f
richegg <zxp86021@gmail.com>
2016-04-15 14:42:07 +0800

    set redirect to moli.rocks when get /

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -12,7 +12,7 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return Redirect::to('https://moli.rocks');
 });
 
 Route::post('message', 'TelegramController@postSendMessage');
