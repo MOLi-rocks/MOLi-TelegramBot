bed416fb446bd36b5ce7b1db75420145593fc579
richegg <zxp86021@gmail.com>
2016-07-22 23:38:27 +0800

    fix code

--- app/Http/Controllers/MOLiBotController.php
+++ app/Http/Controllers/MOLiBotController.php
@@ -21,7 +21,12 @@ class MOLiBotController extends Controller
 
     public function getNCNU_RSS()
     {
-        $fileContents = file_get_contents('http://www.ncnu.edu.tw/ncnuweb/ann/RSS.aspx');
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, 'http://www.ncnu.edu.tw/ncnuweb/ann/RSS.aspx');
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        $fileContents = curl_exec($ch);
+        curl_close($ch);
+
         $formatter = Formatter::make($fileContents, Formatter::XML);
         $json = $formatter->toArray();
         return $json;
