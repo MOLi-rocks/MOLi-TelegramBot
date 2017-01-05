08f4fa9abd43a0f15c56f2e0fb089927860ca89d
richegg <zxp86021@gmail.com>
2017-01-05 14:47:11 +0800

    fix config

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -23,11 +23,11 @@ class Kernel extends HttpKernel
      */
     protected $middlewareGroups = [
         'web' => [
-            \MOLiBot\Http\Middleware\EncryptCookies::class,
-            \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
-            \Illuminate\Session\Middleware\StartSession::class,
-            \Illuminate\View\Middleware\ShareErrorsFromSession::class,
-            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            //\MOLiBot\Http\Middleware\EncryptCookies::class,
+            //\Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
+            //\Illuminate\Session\Middleware\StartSession::class,
+            //\Illuminate\View\Middleware\ShareErrorsFromSession::class,
+            //\Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
         'api' => [
             'throttle:60,1',
