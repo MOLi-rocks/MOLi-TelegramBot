0505063745291c7fba417ffa9ddb6fd41f771d00
zxp86021 <zxp86021@gmail.com>
2017-03-21 22:26:56 +0800

    update Published_KKTIX migration

--- database/migrations/2017_03_21_221710_Published_KKTIX_table.php
+++ database/migrations/2017_03_21_221710_Published_KKTIX_table.php
@@ -13,7 +13,10 @@ class PublishedKKTIXTable extends Migration
      */
     public function up()
     {
-        //
+        Schema::create('Published_KKTIX', function (Blueprint $table) {
+            $table->string('url');
+            $table->primary('url');
+        });
     }
 
     /**
@@ -23,6 +26,6 @@ class PublishedKKTIXTable extends Migration
      */
     public function down()
     {
-        //
+        Schema::drop('Published_KKTIX');
     }
 }
