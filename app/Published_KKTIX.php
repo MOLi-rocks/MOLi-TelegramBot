9c46b5493f36f3425dd0744ad40d2b2566ddc44a
zxp86021 <zxp86021@gmail.com>
2017-04-11 13:34:12 +0800

    fix typo

--- app/Published_KKTIX.php
+++ app/Published_KKTIX.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Published_KKTIX extends Model
 {
+    public $timestamps = false;
+
     protected $table = 'Published_KKTIX';
 
     protected $fillable = ['url', 'title'];
--- app/Published_NCNU_RSS.php
+++ app/Published_NCNU_RSS.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Published_NCNU_RSS extends Model
 {
+    public $timestamps = false;
+
     protected $table = 'Published_NCNU_RSS';
 
     protected $fillable = ['guid', 'title'];
