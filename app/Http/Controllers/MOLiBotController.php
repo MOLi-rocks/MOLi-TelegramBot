809456cb78e846d885c7aa7f22a527a969e38c90
richegg <zxp86021@gmail.com>
2017-06-03 13:36:50 +0800

    add unlimit execute time setting in MOLiBotController get fuel price function

--- app/Http/Controllers/MOLiBotController.php
+++ app/Http/Controllers/MOLiBotController.php
@@ -112,6 +112,9 @@ class MOLiBotController extends Controller
      */
     public function getFuelPrice()
     {
+        // Set unlimit excute time because of slow response from server
+        ini_set('max_execution_time', 0);
+
         $input_xml = '<?xml version="1.0" encoding="utf-8"?><soap12:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap12="http://www.w3.org/2003/05/soap-envelope"><soap12:Body><getCPCMainProdListPrice xmlns="http://tmtd.cpc.com.tw/" /></soap12:Body></soap12:Envelope>';
 
         $ch = curl_init();
