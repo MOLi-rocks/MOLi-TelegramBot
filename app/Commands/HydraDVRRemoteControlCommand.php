82e5373fc868a2142ccf4758588f2a794b7e89d9
richegg <zxp86021@gmail.com>
2017-06-13 12:51:08 +0800

    update HydraDVRRemoteControlCommand

--- app/Commands/HydraDVRRemoteControlCommand.php
+++ app/Commands/HydraDVRRemoteControlCommand.php
@@ -7,6 +7,7 @@ use Telegram\Bot\Commands\Command;
 
 use DB;
 use Telegram;
+use Storage;
 use \GuzzleHttp\Client as GuzzleHttpClient;
 use \GuzzleHttp\Exception\TransferException as GuzzleHttpTransferException;
 
@@ -87,95 +88,35 @@ class HydraDVRRemoteControlCommand extends Command
                         'reply_markup' => $reply_markup
                     ]);
 
-                    $this->control('door');
+                    $this->control('door', $update);
 
                     WhoUseWhatCommand::where('user-id', '=', $update->all()['message']['from']['id'])->delete();
 
                     break;
 
                 case 'Up':
-                    $pic = $this->control('up');
-
-                    if ($pic == env('DVR_SHOT')) {
-                        Telegram::sendPhoto([
-                            'chat_id' => $update->all()['message']['chat']['id'],
-                            'photo' => env('DVR_SHOT'),
-                            'reply_to_message_id' => $update->all()['message']['message_id'],
-                            'disable_notification' => true
-                        ]);
-                    }
-
+                    $this->control('up', $update);
                     break;
 
                 case 'Down':
-                    $pic = $this->control('down');
-
-                    if ($pic == env('DVR_SHOT')) {
-                        Telegram::sendPhoto([
-                            'chat_id' => $update->all()['message']['chat']['id'],
-                            'photo' => env('DVR_SHOT'),
-                            'reply_to_message_id' => $update->all()['message']['message_id'],
-                            'disable_notification' => true
-                        ]);
-                    }
-
+                    $this->control('down', $update);
                     break;
 
                 case 'Left':
-                    $pic = $this->control('left');
-
-                    if ($pic == env('DVR_SHOT')) {
-                        Telegram::sendPhoto([
-                            'chat_id' => $update->all()['message']['chat']['id'],
-                            'photo' => env('DVR_SHOT'),
-                            'reply_to_message_id' => $update->all()['message']['message_id'],
-                            'disable_notification' => true
-                        ]);
-                    }
-
+                    $this->control('left', $update);
                     break;
 
                 case 'Right':
-                    $pic = $this->control('right');
-
-                    if ($pic == env('DVR_SHOT')) {
-                        Telegram::sendPhoto([
-                            'chat_id' => $update->all()['message']['chat']['id'],
-                            'photo' => env('DVR_SHOT'),
-                            'reply_to_message_id' => $update->all()['message']['message_id'],
-                            'disable_notification' => true
-                        ]);
-                    }
-
+                    $this->control('right', $update);
                     break;
 
                 /*
                 case 'Zoom In':
-                    $pic = $this->control('zoomin');
-
-                    if ($pic == env('DVR_SHOT')) {
-                        Telegram::sendPhoto([
-                            'chat_id' => $update->all()['message']['chat']['id'],
-                            'photo' => env('DVR_SHOT'),
-                            'reply_to_message_id' => $update->all()['message']['message_id'],
-                            'disable_notification' => true
-                        ]);
-                    }
-
+                    $this->control('zoomin', $update);
                     break;
 
                 case 'Zoom Out':
-                    $pic = $this->control('zoomout');
-
-                    if ($pic == env('DVR_SHOT')) {
-                        Telegram::sendPhoto([
-                            'chat_id' => $update->all()['message']['chat']['id'],
-                            'photo' => env('DVR_SHOT'),
-                            'reply_to_message_id' => $update->all()['message']['message_id'],
-                            'disable_notification' => true
-                        ]);
-                    }
-
+                    $pic = $this->control('zoomout', $update);
                     break;
                 */
 
@@ -203,7 +144,7 @@ class HydraDVRRemoteControlCommand extends Command
         return response('OK', 200);
     }
 
-    private function control($position) {
+    private function control($position, $update) {
         $client = new GuzzleHttpClient();
 
         try {
@@ -215,8 +156,36 @@ class HydraDVRRemoteControlCommand extends Command
                 'timeout' => 10
             ]);
 
-            return env('DVR_SHOT');
-        } catch (\GuzzleHttp\Exception\TransferException $e) {
+            if ($position != 'door') {
+                $response = $client->request('GET', env('DVR_SHOT'), [
+                    'headers' => [
+                        'User-Agent' => 'MOLi Bot',
+                        'Accept' => 'application/json'
+                    ],
+                    'timeout' => 10
+                ]);
+
+                $type = explode("/", $response->getHeader('Content-Type')[0]);
+
+                $imgpath = Storage::disk('local')->getDriver()->getAdapter()->getPathPrefix();
+
+                if ($type[0] == 'image') {
+                    $fileName = 'HydraDVRRemoteController' . rand(11111, 99999);
+
+                    Storage::disk('local')->put($fileName . '.' . $type[1], $response->getBody());
+
+                    Telegram::sendPhoto([
+                        'chat_id' => $update->all()['message']['chat']['id'],
+                        'reply_to_message_id' => $update->all()['message']['message_id'],
+                        'photo' => $imgpath . $fileName . '.' . $type[1],
+                    ]);
+
+                    Storage::disk('local')->delete($fileName . '.' . $type[1]);
+                }
+            }
+
+            return 'done';
+        } catch (GuzzleHttpTransferException $e) {
             $this->replyWithChatAction(['action' => Actions::TYPING]);
 
             $this->replyWithMessage(['text' => '網路連線異常 QAQ']);
