7ed34ee57c6eea19fc17aff5330d4de5991f8cfe
richegg <zxp86021@gmail.com>
2017-09-21 00:07:26 +0800

    staging

--- config/telegram.php
+++ config/telegram.php
@@ -65,5 +65,6 @@ return [
         MOLiBot\Commands\SearchStaffContactCommand::class,
         MOLiBot\Commands\HydraDVRRemoteControlCommand::class,
         MOLiBot\Commands\MagneticLockStatusCommand::class,
+        MOLiBot\Commands\EnableNCDRCommand::class,
     ],
 ];
