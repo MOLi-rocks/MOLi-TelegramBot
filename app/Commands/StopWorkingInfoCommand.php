80b916786399510ad895e44c844d87d2289b2167
richegg <zxp86021@gmail.com>
2018-04-07 00:31:22 +0800

    颱風價查詢 command 第一版

--- app/Commands/StopWorkingInfoCommand.php
+++ app/Commands/StopWorkingInfoCommand.php
@@ -5,6 +5,9 @@ namespace MOLiBot\Commands;
 use Telegram\Bot\Actions;
 use Telegram\Bot\Commands\Command;
 
+use \GuzzleHttp\Client as GuzzleHttpClient;
+use \GuzzleHttp\Exception\TransferException as GuzzleHttpTransferException;
+
 class StopWorkingInfoCommand extends Command
 {
     /**
@@ -22,9 +25,43 @@ class StopWorkingInfoCommand extends Command
      */
     public function handle($arguments)
     {
+        if (empty(env('NCDR_API_KEY'))) {
+            $this->replyWithChatAction(['action' => Actions::TYPING]);
+            $this->replyWithMessage(['text' => '此服務未啟動']);
+            return response('OK', 200); // 強制結束 command
+        }
+
+        $baseUrl = 'https://alerts.ncdr.nat.gov.tw';
+
+        $client = new GuzzleHttpClient();
+
+        try {
+            $response = $client->request(
+                'GET',
+                $baseUrl . '/api/datastore?format=json&capcode=WSC&apikey=' . env('NCDR_API_KEY'),
+                ['timeout' => 10]
+            );
+        } catch (GuzzleHttpTransferException $e) {
+            $this->replyWithChatAction(['action' => Actions::TYPING]);
+            $this->replyWithMessage(['text' => '暫無停班停課資訊']);
+            return response('OK', 200); // 強制結束 command
+        }
+
+        $info_data = json_decode($response->getBody());
+
         $this->replyWithChatAction(['action' => Actions::TYPING]);
 
-        $this->replyWithMessage(['text' => '我也不知道能不能放假QQ']);
+        if ($info_data['success'] === false || count($info_data['result']) < 1) {
+            $this->replyWithMessage(['text' => '暫無停班停課資訊']);
+        } else {
+            $output_str = '';
+
+            foreach ($info_data['result'] as $result) {
+                $output_str .= trim($result['description']) . PHP_EOL;
+            }
+
+            $this->replyWithMessage(['text' => $output_str]);
+        }
 
         return response('OK', 200);
     }
