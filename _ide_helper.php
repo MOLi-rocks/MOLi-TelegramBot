d1dda564f72e3d705fca63651517a3d5ce332728
richegg <zxp86021@gmail.com>
2018-04-21 10:15:30 +0800

    composer update

--- _ide_helper.php
+++ _ide_helper.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * A helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel 5.5.40 on 2018-04-06 23:23:49.
+ * Generated for Laravel 5.5.40 on 2018-04-21 10:15:03.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
