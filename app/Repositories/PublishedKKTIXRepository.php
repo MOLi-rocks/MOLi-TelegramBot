742e46f4d1147759d583956546fb596f99e17a49
Johnny Tsai <zxp86021@gmail.com>
2019-05-20 16:10:56 +0800

    use service on moli day event

--- app/Repositories/PublishedKKTIXRepository.php
+++ app/Repositories/PublishedKKTIXRepository.php
@@ -6,5 +6,16 @@ use MOLiBot\Models\PublishedKKTIX;
 
 class PublishedKKTIXRepository
 {
-    
+    public function checkEventPublished($url)
+    {
+        return PublishedKKTIX::where('url', '=', $url)->exists();
+    }
+
+    public function storePublishedEvent($event)
+    {
+        return PublishedKKTIX::create([
+            'url' => $event->url,
+            'title' => $event->title
+        ]);
+    }
 }
