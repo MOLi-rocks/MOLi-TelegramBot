221f3d50e2bdf34dbce233d727171e1cc2e004cc
Johnny Tsai <zxp86021@gmail.com>
2019-05-20 15:42:17 +0800

    use service on moli blog article

--- app/Repositories/PublishedMOLiBlogArticleRepository.php
+++ app/Repositories/PublishedMOLiBlogArticleRepository.php
@@ -6,5 +6,17 @@ use MOLiBot\Models\PublishedMOLiBlogArticle;
 
 class PublishedMOLiBlogArticleRepository
 {
-    
+    public function checkArticlePublished($articleId)
+    {
+        return PublishedMOLiBlogArticle::where('id', '=', $articleId)->exists();
+    }
+
+    public function storePublishedArticle($post)
+    {
+        return PublishedMOLiBlogArticle::create([
+            'id' => $post->id,
+            'uuid' => $post->uuid,
+            'title' => $post->title
+        ]);
+    }
 }
