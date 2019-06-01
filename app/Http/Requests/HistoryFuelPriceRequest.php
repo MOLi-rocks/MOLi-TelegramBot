ce4c911f1896bdec9fed78a7dfda79bca519c2b4
Johnny Tsai <zxp86021@gmail.com>
2019-06-01 23:27:24 +0800

    add hint

--- app/Http/Requests/HistoryFuelPriceRequest.php
+++ app/Http/Requests/HistoryFuelPriceRequest.php
@@ -24,6 +24,17 @@ class HistoryFuelPriceRequest extends Request
      */
     public function rules()
     {
+        /*
+        $prodid = array(
+            '1' => '92無鉛汽油',
+            '2' => '95無鉛汽油',
+            '3' => '98無鉛汽油',
+            '4' => '超級/高級柴油',
+            '5' => '低硫燃料油(0.5%)',
+            '6' => '甲種低硫燃料油(0.5)'
+        );
+        */
+
         return [
             'prodid' => 'required|integer|min:1|max:6'
         ];
