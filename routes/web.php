10fc4f5d15fe874c506948bb11c4de57b7718c4e
Johnny Tsai <zxp86021@gmail.com>
2019-06-11 13:52:43 +0800

    move to new route dir

---
+++ routes/web.php
@@ -0,0 +1,45 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', 'MOLiBotController@getIndex');
+
+Route::get('/line-notify-auth', 'LINENotifyController@auth')->name('line_notify_auth');
+
+Route::get('/line-notify-stats', 'LINENotifyController@stats')->name('line_notify_stats');
+
+Route::any('/connect-test', 'MOLiBotController@connectTester');
+
+Route::get('/ncnu-rss', 'MOLiBotController@getNCNU_RSS');
+
+Route::get('/ncdr-rss', 'MOLiBotController@getNCDR_RSS');
+
+Route::get('/ncnu-staff-contact/{keyword?}', 'MOLiBotController@getStaffContact');
+
+Route::get('/fuel-price', 'MOLiBotController@getFuelPrice');
+
+Route::get('/history-fuel-price', 'MOLiBotController@getHistoryFuelPrice');
+
+Route::group(['middleware' => 'bot.token'], function () {
+    Route::post('messages', 'TelegramController@postSendMessage');
+
+    Route::post('photos', 'TelegramController@postSendPhoto');
+
+    Route::post('locations', 'TelegramController@postSendLocation');
+});
+
+Route::post(env('TELEGRAM_BOT_TOKEN'), 'TelegramController@postWebhook');
+
+Route::post(env('NCDR_URL'), 'MOLiBotController@postNCDR');
+
+// keep this route at bottom of file
+Route::any('/{any?}', 'MOLiBotController@anyRoute');
