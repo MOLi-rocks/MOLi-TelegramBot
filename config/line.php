2f2ffefebe65c44efd9bf3156a03968457615206
Johnny Tsai <zxp86021@gmail.com>
2019-06-11 13:18:19 +0800

    update line config

--- config/line.php
+++ config/line.php
@@ -1,19 +1,20 @@
 <?php
 
 return [
+
     /*
     |--------------------------------------------------------------------------
     | LINE Notify Client ID [REQUIRED]
     |--------------------------------------------------------------------------
     |
     | Your LINE Notify's Access Client ID.
-    | Example: adTHJiKiA39mELihBinK
     |
     | Refer for more details:
     | https://notify-bot.line.me/doc/
     |
     */
-    'line_notify_client_id' => env('LINE_NOTIFY_CLIENT_ID', 'YOUR-ID'),
+
+    'line_notify_client_id' => env('LINE_NOTIFY_CLIENT_ID'),
 
     /*
     |--------------------------------------------------------------------------
@@ -21,13 +22,13 @@ return [
     |--------------------------------------------------------------------------
     |
     | Your LINE Notify's Access Client ID.
-    | Example: 3VF2nKo3RdYIUUilOPdUXIZeDfIOPsy0rgSi4
     |
     | Refer for more details:
     | https://notify-bot.line.me/doc/
     |
     */
-    'line_notify_client_secret' => env('LINE_NOTIFY_CLIENT_SECRET', 'YOUR-SECRET'),
+
+    'line_notify_client_secret' => env('LINE_NOTIFY_CLIENT_SECRET'),
 
     /*
     |--------------------------------------------------------------------------
@@ -36,11 +37,12 @@ return [
     |
     | Your LINE Notify's Authentication page URI.
     | The format should be "https://{YOUR_DOMAIN}/auth"
-    | Example: https://api.moli.rocks/line-notify-auth
     |
     | Refer for more details:
     | https://notify-bot.line.me/
     |
     */
-    'line_notify_redirect_uri' => env('LINE_NOTIFY_REDIRECT_URI', 'https://bot.moli.rocks/line-notify-auth'),
+
+    'line_notify_redirect_uri' => env('LINE_NOTIFY_REDIRECT_URI'),
+
 ];
