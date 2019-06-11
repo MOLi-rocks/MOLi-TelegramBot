1cf493623fef0e5f482a117b941e8e76be244d1c
Johnny Tsai <zxp86021@gmail.com>
2019-06-11 13:07:01 +0800

    update cors config

--- config/cors.php
+++ config/cors.php
@@ -1,22 +1,29 @@
 <?php
 
 return [
+
     /*
      |--------------------------------------------------------------------------
      | Laravel CORS
      |--------------------------------------------------------------------------
      |
-
      | allowedOrigins, allowedHeaders and allowedMethods can be set to array('*')
      | to accept any value.
      |
      */
+
     'supportsCredentials' => false,
+
     'allowedOrigins' => ['*'],
+
     'allowedHeaders' => ['Content-Type', 'Accept'],
+
     'allowedMethods' => ['GET', 'POST', 'PUT',  'DELETE'],
+
     'exposedHeaders' => [],
+
     'maxAge' => 0,
+    
     'hosts' => [],
 ];
 
