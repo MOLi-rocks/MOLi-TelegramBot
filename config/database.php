536a9dab8a24c395b1609d0e9f3f60666c6b85ac
Johnny Tsai <zxp86021@gmail.com>
2019-07-15 10:53:26 +0800

    set config

--- config/database.php
+++ config/database.php
@@ -78,6 +78,21 @@ return [
             'prefix' => '',
         ],
 
+        'testing' => [
+            'driver' => 'mysql',
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => 'moli_bot_testing',
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'strict' => true,
+            'engine' => null,
+        ],
+
     ],
 
     /*
