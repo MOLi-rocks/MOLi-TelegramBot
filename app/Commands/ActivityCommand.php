b3d943effe163084eb36f78e75b8f48b037f5a6c
Johnny Tsai <zxp86021@gmail.com>
2019-07-16 16:23:33 +0800

    處理 kktix 拿不到資料的狀況

--- app/Commands/ActivityCommand.php
+++ app/Commands/ActivityCommand.php
@@ -2,12 +2,11 @@
 
 namespace MOLiBot\Commands;
 
+use Exception;
 use Telegram\Bot\Actions;
 use Telegram\Bot\Commands\Command;
 
-use Telegram;
-use \GuzzleHttp\Client as GuzzleHttpClient;
-use \GuzzleHttp\Exception\TransferException as GuzzleHttpTransferException;
+use MOLiBot\DataSources\MoliKktix;
 
 class ActivityCommand extends Command
 {
@@ -26,43 +25,35 @@ class ActivityCommand extends Command
      */
     public function handle($arguments)
     {
-        $client = new GuzzleHttpClient();
+        $kktix = new MoliKktix();
 
         try {
-            $response = $client->request('GET', 'https://moli.kktix.cc/events.json', [
-                'headers' => [
-                    'User-Agent' => 'MOLi Bot',
-                    'Accept'     => 'application/json'
-                ],
-                'timeout' => 10
-            ]);
-        } catch (GuzzleHttpTransferException $e) {
+            $events = $kktix->getContent();
+
+            $activity = 0;
+
+            foreach ($events['entry'] as $num => $detail) {
+                if ( strtotime($detail['published']) > strtotime('now') ) {
+                    $this->replyWithChatAction(['action' => Actions::TYPING]);
+
+                    $this->replyWithMessage([
+                        'text' => $detail['title'] . PHP_EOL . '' . PHP_EOL . $detail['content'] . PHP_EOL . '' . PHP_EOL . $detail['url']
+                    ]);
+
+                    $activity++;
+                } else break;
+            }
+
+            if ($activity == 0) {
+                $this->replyWithChatAction(['action' => Actions::TYPING]);
+                $this->replyWithMessage(['text' => '最近無排定活動，歡迎在群組挖坑' . PHP_EOL . 'https://www.facebook.com/groups/MOLi.rocks']);
+            }
+
+            return response('OK', 200);
+        } catch (Exception $e) {
             $this->replyWithChatAction(['action' => Actions::TYPING]);
-            $this->replyWithMessage(['text' => '網路連線異常 QAQ']);
+            $this->replyWithMessage(['text' => '壞惹 QAQ']);
             return response('OK', 200); // 強制結束 command
         }
-
-        $body = $response->getBody();
-        $json = json_decode($body, true);
-        $activity = 0;
-
-        foreach ($json['entry'] as $num => $detail) {
-            if ( strtotime($detail['published']) > strtotime('now') ) {
-                $this->replyWithChatAction(['action' => Actions::TYPING]);
-
-                $this->replyWithMessage([
-                    'text' => $detail['title'] . PHP_EOL . '' . PHP_EOL . $detail['content'] . PHP_EOL . '' . PHP_EOL . $detail['url']
-                ]);
-
-                $activity++;
-            } else break;
-        }
-
-        if ($activity == 0) {
-            $this->replyWithChatAction(['action' => Actions::TYPING]);
-            $this->replyWithMessage(['text' => '最近無排定活動，歡迎在群組挖坑' . PHP_EOL . 'https://www.facebook.com/groups/MOLi.rocks']);
-        }
-
-        return response('OK', 200);
     }
 }
