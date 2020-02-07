3deae903ba8f33d447d4f8a4f529c53a7c76abfb
Johnny Tsai <zxp86021@gmail.com>
2020-02-07 10:57:46 +0800

    update ide-helper file

--- app/Models/WelcomeMessageRecord.php
+++ app/Models/WelcomeMessageRecord.php
@@ -4,6 +4,24 @@ namespace MOLiBot\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * MOLiBot\Models\WelcomeMessageRecord
+ *
+ * @property int $chat_id
+ * @property int $member_id new member id
+ * @property int $welcome_message_id
+ * @property int $checked
+ * @property int $join_at unix-timestamp of join time
+ * @method static \Illuminate\Database\Eloquent\Builder|\MOLiBot\Models\WelcomeMessageRecord newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\MOLiBot\Models\WelcomeMessageRecord newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\MOLiBot\Models\WelcomeMessageRecord query()
+ * @method static \Illuminate\Database\Eloquent\Builder|\MOLiBot\Models\WelcomeMessageRecord whereChatId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\MOLiBot\Models\WelcomeMessageRecord whereChecked($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\MOLiBot\Models\WelcomeMessageRecord whereJoinAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\MOLiBot\Models\WelcomeMessageRecord whereMemberId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\MOLiBot\Models\WelcomeMessageRecord whereWelcomeMessageId($value)
+ * @mixin \Eloquent
+ */
 class WelcomeMessageRecord extends Model
 {
     public $timestamps = false;
