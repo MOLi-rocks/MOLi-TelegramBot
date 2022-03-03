62f62d5ab01063acc3f15bb384d1d60b80418da8
richegg Tsai <3607700+zxp86021@users.noreply.github.com>
2022-03-03 10:32:03 +0800

    modify ncnu_staff_contact (#130)

--- app/Traits/GetHelpTrait.php
+++ app/Traits/GetHelpTrait.php
@@ -14,7 +14,7 @@ trait GetHelpTrait
         $text = '';
 
         //不想顯示在 help 及 start 的指令請填在這個陣列
-        $hidden = ['start', 'whoami', 'DVRremoteController', 'ncdrstart', 'MagneticLockStatus', 'fuck'];
+        $hidden = ['start', 'DVRremoteController', 'ncdrstart', 'MagneticLockStatus', 'fuck', 'staffcontact'];
 
         $commands = Arr::except($commands, $hidden);
 
@@ -22,7 +22,7 @@ trait GetHelpTrait
             $text .= sprintf('/%s - %s'.PHP_EOL, $name, $handler->getDescription());
         }
 
-        $text .= sprintf('Hints: ' . PHP_EOL);
+        $text .= sprintf(PHP_EOL . 'Hints: ' . PHP_EOL);
         $text .= sprintf('1. 加入 MOLi 廣播頻道 ( @MOLi_Channel ) 以獲得即時開關門資訊' . PHP_EOL);
         $text .= sprintf('2. 加入"非官方"暨大最新公告 ( @NCNU_NEWS ) 以快速獲得校內最新公告資訊' . PHP_EOL);
         $text .= sprintf('3. MOLi 天氣廣播 ( @MOLi_Weather ) 已開台' . PHP_EOL);
